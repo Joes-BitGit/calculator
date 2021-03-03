28303356964a5ba88ce365b1cbabce1aa030b2a2
Joes-BitGit <josephnalmeida@gmail.com>
2021-03-03 15:46:47 -0800

    added classes to help w/ styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import style from './styles.css'
+import './styles.css';
 
 function App() {
   return (
-    <div className="grid-container">
-      <div id="display">DISPLAY</div>
-      <button id="clear">AC</button>
-      <button id="divide" className="operator">/</button>
-      <button id="multiply" className="operator">X</button>
-      <button id="seven" className="number">7</button>
-      <button id="eight" className="number">8</button>
-      <button id="nine" className="number">9</button>
-      <button id="subtract" className="operator">-</button>
-      <button id="four" className="number">4</button>
-      <button id="five" className="number">5</button>
-      <button id="six" className="number">6</button>
-      <button id="add" className="operator">+</button>
-      <button id="one" className="number">1</button>
-      <button id="two" className="number">2</button>
-      <button id="three" className="number">3</button>
-      <button id="equals">=</button>
-      <button id="zero" className="number">0</button>
-      <button id="decimal" className="number">.</button>
+    <div className="calculator-container">
+      <div id="display" className="output">DISPLAY</div>
+      <button id="clear" className="section">AC</button>
+      <button id="divide" className="operator section">/</button>
+      <button id="multiply" className="operator section">x</button>
+      <button id="seven" className="number section">7</button>
+      <button id="eight" className="number section">8</button>
+      <button id="nine" className="number section">9</button>
+      <button id="subtract" className="operator section">-</button>
+      <button id="four" className="number section">4</button>
+      <button id="five" className="number section">5</button>
+      <button id="six" className="number section">6</button>
+      <button id="add" className="operator section">+</button>
+      <button id="one" className="number section">1</button>
+      <button id="two" className="number section">2</button>
+      <button id="three" className="number section">3</button>
+      <button id="equals" className="section">=</button>
+      <button id="zero" className="number section">0</button>
+      <button id="decimal" className="number section">.</button>
     </div>
   );
 }
