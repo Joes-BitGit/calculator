61285349683df3e5d532d4b4c303c711baed37dd
Joes-BitGit <josephnalmeida@gmail.com>
2021-03-05 17:24:38 -0800

    Event Handler for each button & input state

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
 function App() {
+  // State of the current button pressed
+  const [input, setInput] = useState("");
+
+  const handleClick = (e) => {
+    // save this state in order to display in the output
+    console.log(e.target.textContent);
+    setInput(`${input} ${e.target.textContent}`);
+  }
+
   return (
     <div className="calculator-container">
-      <div id="display" className="output">DISPLAY</div>
-      <button id="clear" className="section">AC</button>
-      <button id="divide" className="operator section">/</button>
-      <button id="multiply" className="operator section">x</button>
-      <button id="seven" className="number section">7</button>
-      <button id="eight" className="number section">8</button>
-      <button id="nine" className="number section">9</button>
-      <button id="subtract" className="operator section">-</button>
-      <button id="four" className="number section">4</button>
-      <button id="five" className="number section">5</button>
-      <button id="six" className="number section">6</button>
-      <button id="add" className="operator section">+</button>
-      <button id="one" className="number section">1</button>
-      <button id="two" className="number section">2</button>
-      <button id="three" className="number section">3</button>
-      <button id="equals" className="section">=</button>
-      <button id="zero" className="number section">0</button>
-      <button id="decimal" className="number section">.</button>
+      <div id="display" className="output">{input}</div>
+      <button id="clear" className="section" onClick={handleClick}>AC</button>
+      <button id="divide" className="operator section" onClick={handleClick}>/</button>
+      <button id="multiply" className="operator section" onClick={handleClick}>x</button>
+      <button id="seven" className="number section" onClick={handleClick}>7</button>
+      <button id="eight" className="number section" onClick={handleClick}>8</button>
+      <button id="nine" className="number section" onClick={handleClick}>9</button>
+      <button id="subtract" className="operator section" onClick={handleClick}>-</button>
+      <button id="four" className="number section" onClick={handleClick}>4</button>
+      <button id="five" className="number section" onClick={handleClick}>5</button>
+      <button id="six" className="number section" onClick={handleClick}>6</button>
+      <button id="add" className="operator section" onClick={handleClick}>+</button>
+      <button id="one" className="number section" onClick={handleClick}>1</button>
+      <button id="two" className="number section" onClick={handleClick}>2</button>
+      <button id="three" className="number section" onClick={handleClick}>3</button>
+      <button id="equals" className="section" onClick={handleClick}>=</button>
+      <button id="zero" className="number section" onClick={handleClick}>0</button>
+      <button id="decimal" className="number section" onClick={handleClick}>.</button>
     </div>
   );
 }
